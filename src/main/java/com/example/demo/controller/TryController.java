1732eb63ab6658a5106d42be17f5962d9aff0444
dongchuanbi <dongchuanbi@126.com>
2019-10-10 11:34:55 +0800

    try controller

---
+++ src/main/java/com/example/demo/controller/TryController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/try")
+public class TryController {
+
+	@GetMapping("/tryDemo")
+	public String tryDemo(){
+		return "给你demo啊！";
+	}
+}
